17e0c413f01d5cf0927b1497bccb42dcb2d60616
antonovmike <antonovma@outlook.com>
2024-10-07 08:58:40 +0200

    [IMP] create_board moved to board.py

--- board.py
+++ board.py
@@ -1,3 +1,6 @@
+from pieces import Bishop, ChessPiece, King, Knight, Pawn, Rook, Queen, piece_names
+
+
 class Cell:
     def __init__(self, color: str, piece=None):
         self.color = color
@@ -8,3 +11,27 @@ class Cell:
             return self.color
         else:
             return self.color[0] + self.piece.__repr__() + self.color[1:]
+
+
+def create_board():
+    pieces_order = ["r", "h", "b", "q", "k", "b", "h", "r"]
+    empty_order = [None] * 8
+
+    board = []
+    for row_index in range(8):
+        row = []
+        for col_index in range(8):
+            color = ":::" if (row_index + col_index) % 2 == 0 else "   "
+            if row_index == 0:
+                piece = globals()[piece_names[pieces_order[col_index].upper()]]("upper")
+            elif row_index == 1:
+                piece = Pawn("upper", board)
+            elif row_index >= 6:
+                piece = Pawn("lower", board)
+                if row_index == 7:
+                    piece = globals()[piece_names[pieces_order[col_index]]]("lower")
+            else:
+                piece = empty_order[col_index]
+            row.append(Cell(color, piece))
+        board.append(row)
+    return board
--- main.py
+++ main.py
@@ -1,43 +1,19 @@
 from player import Player
-from board import Cell
-from pieces import Bishop, ChessPiece, King, Knight, Pawn, Rook, Queen, piece_names
+from board import create_board
+from pieces import Pawn
 
 
 player_1 = Player("Player 1", "lower")
 player_2 = Player("Player 2", "upper")
 
 
+first_board = create_board()
+
+
 def count_distance(source_row: int, source_col: int, target_row: int, target_col: int):
     horizontal = source_row - target_row
     vertical = source_col - target_col
     print('==>>', horizontal, vertical)
-
-
-def create_board():
-    pieces_order = ["r", "h", "b", "q", "k", "b", "h", "r"]
-    empty_order = [None] * 8
-
-    board = []
-    for row_index in range(8):
-        row = []
-        for col_index in range(8):
-            color = ":::" if (row_index + col_index) % 2 == 0 else "   "
-            if row_index == 0:
-                piece = globals()[piece_names[pieces_order[col_index].upper()]]("upper")
-            elif row_index == 1:
-                piece = Pawn("upper", board)
-            elif row_index >= 6:
-                piece = Pawn("lower", board)
-                if row_index == 7:
-                    piece = globals()[piece_names[pieces_order[col_index]]]("lower")
-            else:
-                piece = empty_order[col_index]
-            row.append(Cell(color, piece))
-        board.append(row)
-    return board
-
-
-first_board = create_board()
 
 
 def move_piece():
